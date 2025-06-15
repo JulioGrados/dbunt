c26ff3ea105d8c3564b19d54ea8d8c04e54b8018
Julio Giampiere Grados Caballero <juliogiampieregradoscaballero@Mac-mini-de-Julio.local>
2025-06-14 20:25:34 -0500

    cambios evaluation

---
+++ models/evaluation.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const timestamps = require('mongoose-timestamp')
+const mongooseBeautifulUniqueValidation = require('mongoose-beautiful-unique-validation')
+
+const EvaluationSchema = new Schema(
+  {
+    title: {
+      type: String,
+    },
+    position: {
+        type: String,
+    },
+    experiment: {
+        type: String,
+    },
+    process: {
+        type: String,
+        default: 'Testing'
+    },
+    processWhy: {
+        type: String
+    },
+    dont: {
+        type: Number,
+        default: 0
+    },
+    nice: {
+        type: Number,
+        default: 0
+    },
+    must: {
+        type: Number,
+        default: 0
+    },
+    file: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    parent: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'Evaluation', 
+        default: null 
+    }
+  },
+  {
+    collection: 'evaluations'
+  }
+)
+
+EvaluationSchema.plugin(timestamps, {
+  createdAt: { index: true },
+  updatedAt: { index: true }
+})
+
+EvaluationSchema.plugin(mongooseBeautifulUniqueValidation)
+
+EvaluationSchema.index({
+  title: 1
+})
+
+module.exports = mongoose.model('Evaluation', EvaluationSchema)
